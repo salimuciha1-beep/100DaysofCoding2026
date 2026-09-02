fb72b9c40bc968b732303c8717d99bdc7c95650f
salimuciha1-beep <salim.uciha1@gmail.com>
2026-09-02 20:49:50 +0700

    Create Day1.java

---
+++ Day1.java
@@ -0,0 +1,20 @@
+public class day1{
+    public static void main(String[] args) {
+        // Ini adalah komentar satu baris
+System.out.println("Hallo world"); // Komentar di akhir baris kode
+
+/* 
+  Ini adalah komentar multi-baris.
+  Bisa ditulis lebih dari satu baris.
+*/
+
+/**
+ * Ini adalah komentar dokumentasi
+ * untuk menjelaskan fungsi kelas atau metode.
+ */
+
+
+        String nama = "muhammad mursalim";
+        System.out.println(nama);
+    }
+}
