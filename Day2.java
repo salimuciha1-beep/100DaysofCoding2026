43775a70f2484cf9fd78053de7f4b80246e64677
salimuciha1-beep <salim.uciha1@gmail.com>
2026-09-03 17:31:17 +0700

    Refactor Day2.java to clean up code
    Removed unused variables and updated print statements.

--- Day2.java
+++ Day2.java
@@ -1,28 +1,23 @@
 public class Day2 {
     public static void main(String[] args) {
         String nama = "muhammad mursalim";
-        String tempatlahir = "lembang bunga";
+        String nim = "D0225028";
         int umur = 19;
-        double tinggibadan = 170.5;
+        String jurusan = "INFORMATIKA";
 
-
-        // 1. println() - menampilkan teks dan otomatis pidah baris baru.
-        System.out.println("=== biodata diri ===");
+        System.out.println("====================");
+        System.out.println("|  biodata diri    |" );
+        System.out.println("====================");
         System.out.println("nama     :" + nama);
-
-
-        // 2. print() - menampilkan teks tampa pinda baris.
-        System.out.print("tempat lahir :");
-        System.out.print(tempatlahir);
-        System.out.print("\n");// menggunakan \n untuk pindah baris secara manual
-
-
-        // 3. printf() - menampilkan teks dengan format variabel
-        // %d = integer (angka bulat), %.1f = float/dooble (1 angka di belakang koma)
         
-        System.out.printf("umur     : %d tahun %n", umur  );
-        System.out.printf("tinggi badan  : %.1f cm%n ", tinggibadan);
+        System.out.printf("NIM      : %s %numur     : %d \n", nim, umur);
+        
 
+        System.out.print("jurusan  : ");
+        System.out.print(jurusan);
+
+
+        
     }
     
 }
