10feb362e3f549a33d6bd23a5dbf7d380d33bde9
salimuciha1-beep <salim.uciha1@gmail.com>
2026-09-04 16:00:14 +0700

    Create Day3 class to display bookstore information

---
+++ Day.java
@@ -0,0 +1,24 @@
+public class Day3 {
+    public static void main(String[] args) {
+        System.out.println("=============================");
+        System.out.println(" \ttoko buku  : \"garuda\"");
+        System.out.println("=============================\n");
+
+        System.out.println("promo hari ini: \"beli 2 gratis 1\"");
+         
+        System.out.println("item\t harga");
+        System.out.println("novel\t Rp.55.000");
+        System.out.println("komik\t Rp.25.000");
+        System.out.println("atlas\t Rp.80.000\n\n");
+
+        System.out.println("file struk disimpan di: D:\\toko\\struk");
+        System.out.println("terimah kasih sudah belanja di toko buku garuda");
+        
+// \t  berfungsi sebagai Tab
+// \n berfungsi sebagai penambah baris baru
+// \" untuk menampilkan tanpa mebuat kode rusak
+// \\ ini untuk menampil kan satu karkter saja(\)
+
+    }
+    
+}
