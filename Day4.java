e1eb526dec1354615e8aaa47da0b7ffd3206caa3
salimuciha1-beep <salim.uciha1@gmail.com>
2026-09-05 14:02:25 +0700

    Create Day4.java with variable type examples
    Implement Day4 class with main method demonstrating variable types.

---
+++ Day4.java
@@ -0,0 +1,20 @@
+public class Day4 {
+    public static void main(String[] args) {
+        int n = 200;
+        short p = 30;
+        byte i = 70;
+        long y = 1000;
+
+        System.out.println("nilai dari int  :" + n);
+        System.out.println("nilai dari short  :" + p);
+        System.out.println("nilai dari byte  :" + i);
+        System.out.println("nilai dari long :" + y);
+
+        // int yang paling sering di pakai di java
+        // short dan byte cocok dipakai untuk menghemat memori 
+        // long digunakan apabila nilai di int sudah memsenuhi kapasitas
+        
+        
+    }
+    
+}
