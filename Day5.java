ad13dc543f477640a6acff4d54e84eb2f0433d9f
salimuciha1-beep <salim.uciha1@gmail.com>
2026-09-06 15:56:48 +0700

    Create Day5.java to display weight and height
    Added a Java class with main method to display weight and height.

---
+++ Day5.java
@@ -0,0 +1,13 @@
+public class Day5 {
+
+    public static void main(String[] args) {
+        float bb = 80.3f;
+        double tb = 170.3;
+
+        System.out.println("berat badan  : " + bb);
+        System.out.println("tinggi badan : " + tb);
+
+        //float biasa dugunakan ketika bilangan pecahannya rendah dan akhiranya harus ada huruh f karena supaya bisa terbaca sebagai float
+        //double ini tipe data yang sering digunakan ketika kita ingin menampilkan desimal tanpa harus menambahkan sesuatu seperti float.
+    }
+}
