c00058f10f257af77c1af6effabece4676df9caf
salimuciha1-beep <salim.uciha1@gmail.com>
2026-09-07 11:31:02 +0700

    Add Day6 class with main method and examples

---
+++ Day6.java
@@ -0,0 +1,13 @@
+public class Day6 {
+    public static void main(String[] args) {
+        char awlan = 'm';
+        boolean benar = true;
+        
+        System.out.println("menampilkan benar :" + benar);
+        System.out.println("awalan nama saya :" + awlan); 
+
+        // char bisa digunakan apa bila kita menggunakan tanda satu seperti ini 'm' dan char juga hanya bisa menamilkan 1 huruf.
+        //boolean hanyan mengenal true end false ini di gunakan untuk mengambil keputusan.
+    }
+    
+}
