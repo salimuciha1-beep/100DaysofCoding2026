7d590f91fef32ac8f83a17f9fd594a479dad959f
salimuciha1-beep <salim.uciha1@gmail.com>
2026-09-07 23:17:04 +0700

    Add Day7.java with a simple introduction program

---
+++ Day7.java
@@ -0,0 +1,10 @@
+public class Day7 {
+    public static void main(String[] args) {
+        String nama = "muhammad mursalim";
+
+        System.out.println("perkenalkan nama saya  :  " + nama);
+
+      //string sering digunakan untuk memasukkan teks dalam sebuah java
+        
+    }
+}
