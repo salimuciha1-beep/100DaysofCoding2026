ba81a607097052a3abf4a67acead08b62027a119
salimuciha1-beep <salim.uciha1@gmail.com>
2026-09-09 18:40:11 +0700

    Create Day8.java

---
+++ Day8.java
@@ -0,0 +1,25 @@
+public class Day8 {
+    public static void main(String[] args) {
+        int a;
+        double b;
+
+        a = 30;
+        b = 12.4;
+
+        int c = 100;
+
+        System.out.println("umur : " + a);
+        System.out.println("tg : " + b);
+        System.out.println("nilai : " + c);
+
+        a = 40;
+        b = 14.5;
+
+        c = c + 50;
+
+         System.out.println("\numur saya : " + a);
+        System.out.println("tg saya : " + b);
+        System.out.println("nilai saya :"  + c);
+
+    }
+}
