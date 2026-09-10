874ab35e6439c704e428e325e94db7808437aea7
salimuciha1-beep <salim.uciha1@gmail.com>
2026-09-10 11:14:29 +0700

    Add Day9 class with main method and constants

---
+++ Day9.java
@@ -0,0 +1,12 @@
+public class Day9 {
+    public static void main(String[] args) {
+        
+        final double PI = 3.14;
+        final int MAX_SPEED = 3;
+        final String NAMA_PERUSAHAAN = "pt cinta abadi";
+
+        System.out.println("maksimal    :" + PI);
+        System.out.println("nilai    :" + MAX_SPEED);
+        System.out.println("Nama perusahan    :" + NAMA_PERUSAHAAN );
+    }
+}
