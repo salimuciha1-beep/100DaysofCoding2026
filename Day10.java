33bd96596685a957a8f6d763752bdbc93de80d51
salimuciha1-beep <salim.uciha1@gmail.com>
2026-09-11 16:10:16 +0700

    Add Day10.java to display primitive type limits

---
+++ Day10.java
@@ -0,0 +1,20 @@
+public class Day10 {
+    public static void main(String[] args) {
+        System.out.println("BYTE  :" + Byte.MIN_VALUE);
+        System.out.println("BYTE  :" + Byte.MAX_VALUE);
+        System.out.println("integer  :" + Integer.MIN_VALUE);
+        System.out.println("integer  :" + Integer.MAX_VALUE);
+        System.out.println("SHORT  :" + Short.MIN_VALUE);
+        System.out.println("SHORT  :" + Short.MAX_VALUE);
+        System.out.println("LONG  :" + Long.MIN_VALUE);
+        System.out.println("LONG  :" + Long.MAX_VALUE);
+        System.out.println("FLOAT  :" + Float.MIN_VALUE);
+        System.out.println("FLOAT :" + Float.MAX_VALUE);
+        System.out.println("DOUBLE  :" + Double.MIN_VALUE);
+        System.out.println("DOUBLE :" + Double.MAX_VALUE);
+
+        //jika kita ingin menampilkan nilai minimum kita pakai MIN_VALUE
+        //jika kita ingin menampilkan nilai maksimum kita pakai MAX_VALUE
+        
+    }
+}
