a316dae87debc926bbd6f8a1eed8f4e33657611d
salimuciha1-beep <salim.uciha1@gmail.com>
2026-09-15 19:46:12 +0700

    Create Day14.java

---
+++ Day14.java
@@ -0,0 +1,17 @@
+public class Day14 {
+    public static void main(String[] args) {
+        int angka1 = 10;
+        int angka2 = 5;
+        int hasil = angka1+angka2;
+          System.out.println("hasil penjumlahan = " + hasil);
+
+        int angka01 = 10;
+        int angka02 = 20;
+        int hasil2 = angka1-angka2;
+
+    
+        System.out.println("hasil penjumlahan = " + hasil2);
+
+
+    }
+}
