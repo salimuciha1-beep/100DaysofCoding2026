b80349b32ec69a60088490f44d5fd11c3217945e
salimuciha1-beep <salim.uciha1@gmail.com>
2026-09-19 21:03:40 +0700

    Add Day18.java with type conversion example

---
+++ Day18.java
@@ -0,0 +1,18 @@
+public class Day18 {
+    public static void main(String[] args) {
+      
+    byte a = 15;
+    short b = a;
+    int c = b;
+    long d = c;
+    float e =  d;
+    double f = e;
+
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+    System.out.println(d);
+    System.out.println(e);
+    System.out.println(f);
+    }
+}
