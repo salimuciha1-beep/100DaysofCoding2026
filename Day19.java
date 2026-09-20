3e67eed51810d9b95bd59d39cc882306ed5b256e
salimuciha1-beep <salim.uciha1@gmail.com>
2026-09-20 21:11:27 +0700

    Add Day19.java with basic arithmetic operations

---
+++ Day19.java
@@ -0,0 +1,9 @@
+public class Day19 {
+    public static void main(String[] args) {
+       int a = 5, b = 2;
+       double x = a /b * 2.0;
+       double y= a * 2.0 / b;
+       System.out.println(x + "dan" + y);
+
+    }
+}
