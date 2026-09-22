15ec3cd6257817e7efeaea3fd5cbb24336f23e64
salimuciha1-beep <salim.uciha1@gmail.com>
2026-09-22 19:54:01 +0700

    Add Day21.java to parse and print integers

---
+++ Day21.java
@@ -0,0 +1,11 @@
+public class Day21 {
+    public static void main(String[] args) {
+        String a ="100";
+        String b = "100000";
+
+        int sa = Integer.parseInt(a);
+        long bs = Long.parseLong(b);
+        System.out.println(sa);
+        System.out.println(bs);
+    }
+}
