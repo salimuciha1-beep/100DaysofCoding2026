019d6cabc66f02661e915c4f377de4665080110e
salimuciha1-beep <salim.uciha1@gmail.com>
2026-09-24 21:07:18 +0700

    Add Day23 class to calculate area and perimeter

---
+++ Day23.java
@@ -0,0 +1,11 @@
+public class Day23 {
+    public static void main(String[] args) {
+        int sisi =  5;
+        int luas = 8;
+        int keliling = sisi * luas;
+        int hasil = 2 * (sisi + luas);
+
+         System.out.println(keliling);
+         System.out.println(hasil); 
+    }
+}
