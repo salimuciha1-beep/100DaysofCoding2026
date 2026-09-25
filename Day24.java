007f6cbdcaebd681c68723ce9e29bc8b60502b1b
salimuciha1-beep <salim.uciha1@gmail.com>
2026-09-25 19:19:00 +0700

    Implement perimeter calculation in Day24.java
    Calculate the perimeter of a rectangle using side length and width.

---
+++ Day24.java
@@ -0,0 +1,14 @@
+public class Day24 {
+    public static void main(String[] args) {
+        int sisi =  5;
+        int luas = 4;
+        int keliling = sisi * luas;
+        
+
+         System.out.println(keliling);
+    
+    }
+}
+
+    
+
