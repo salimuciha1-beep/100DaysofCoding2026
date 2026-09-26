95a90aa88614dbe83d6206a8c261006b55ff3c6f
salimuciha1-beep <salim.uciha1@gmail.com>
2026-09-26 20:26:41 +0700

    Add Day25.java to calculate area and circumference

---
+++ Day25.java
@@ -0,0 +1,9 @@
+public class Day25 {
+    public static void main(String[] args) {
+        double luas = 20;
+        double keliling = Math.PI * luas * luas;
+
+        System.out.println("luas keliling" + luas);
+        System.out.printf("luas   = %.2f %n", keliling);
+    }
+}
